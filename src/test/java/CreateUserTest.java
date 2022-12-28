147596c1bd8a9d3ba4459604de31c5eb9a19a1e9
DmitriyKnol <Dmi3ydik@yandex.ru>
2022-12-28 22:57:32 +0500

    create CreateUserTest

---
+++ src/test/java/CreateUserTest.java
@@ -0,0 +1,146 @@
+import com.github.javafaker.Faker;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+public class CreateUserTest {
+    AccessTokenBearer accessTokenBearer;
+    Faker faker = new Faker();
+    private final String emailFaker = faker.internet().emailAddress();
+    private final String passwordFaker = faker.internet().password(6,10);
+    private final String userNameFaker = faker.name().firstName();
+    CreateUser createUser = new CreateUser(emailFaker, passwordFaker, userNameFaker);
+    CreateUser createUserWithoutEmail = new CreateUser("", passwordFaker, userNameFaker);
+    CreateUser createUserWithoutPassword = new CreateUser(emailFaker, "", userNameFaker);
+    CreateUser createUserWithoutUserName = new CreateUser(emailFaker, passwordFaker, "");
+    DeleteUser deleteUser = new DeleteUser(emailFaker, passwordFaker);
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
+    }
+
+    @Test
+    @DisplayName("Проверка возможности создать нового пользователя")
+    @Description("Проверяет позитивную попытку создать пользователя")
+    public void successfulRegistrationNewUser() {
+        accessTokenBearer = given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUser)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(200)
+                .body("success", is(true))
+                .extract().body().as(AccessTokenBearer.class);
+
+    }
+
+    @Test
+    @DisplayName("Проверка возможности создать нового пользователя")
+    @Description("Проверяет попытку создать пользователя с повторяющимся именем")
+    public void registrationDuplicateUser() {
+        accessTokenBearer = given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUser)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .extract().body().as(AccessTokenBearer.class);
+
+        given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUser)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(403)
+                .body("success", is(false));
+
+    }
+
+    @Test
+    @DisplayName("Проверка возможности создать нового пользователя")
+    @Description("Проверяет возможность создания пользователя без заполненного email")
+    public void registrationUserWithoutEmail() {
+        accessTokenBearer = given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUserWithoutEmail)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(403)
+                .body("success", is(false))
+                .extract().body().as(AccessTokenBearer.class);
+
+    }
+
+    @Test
+    @DisplayName("Проверка возможности создать нового пользователя")
+    @Description("Проверяет возможность создания пользователя без заполненного пароля")
+    public void registrationUserWithoutPassword() {
+        accessTokenBearer = given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUserWithoutPassword)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(403)
+                .body("success", is(false))
+                .extract().body().as(AccessTokenBearer.class);
+
+    }
+
+    @Test
+    @DisplayName("Проверка возможности создать нового пользователя")
+    @Description("Проверяет возможность создания пользователя без заполненного имени пользователя")
+    public void registrationUserWithoutUserName() {
+        accessTokenBearer = given()
+                .header("Content-type", "application/json")
+                .log().body()
+                .body(createUserWithoutUserName)
+                .when()
+                .post("/api/auth/register")
+                .then()
+                .log().body()
+                .assertThat()
+                .statusCode(403)
+                .body("success", is(false))
+                .extract().body().as(AccessTokenBearer.class);
+
+    }
+
+    @After
+    public void deleteUser() {
+    if (accessTokenBearer.getAccessToken() != null) {
+    given()
+            .auth().oauth2(accessTokenBearer.getAccessToken().substring(7, accessTokenBearer.getAccessToken().length()))
+            .header("Content-type", "application/json")
+            .log().body()
+            .body(deleteUser)
+            .when()
+            .delete("/api/auth/user")
+            .then()
+            .log().body();
+}
+    }
+}
